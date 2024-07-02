95d0e27e8f7b658e9ffc0dce02ef3272a7499cef
Ajith Kumar S <ajithkumars1609@gmail.com>
2024-07-02 08:43:24 +0530

    Updated the env mongo url

--- Config/db.js
+++ Config/db.js
@@ -1,14 +1,14 @@
 import chalk from 'chalk';
 import mongoose from 'mongoose';
 
-const MONGO_URI = process.env.DATABASE.replace(
+const DB_URL = process.env.MONGO_URL.replace(
     '<PASSWORD>',
-    process.env.DATABASE_PASSWORD,
+    process.env.MONGO_URL_PASSWORD,
 );
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(MONGO_URI);
+        await mongoose.connect(DB_URL);
         console.log(chalk.green.italic.bold('MongoDB connected ....'));
     } catch (err) {
         console.log(err.message);
